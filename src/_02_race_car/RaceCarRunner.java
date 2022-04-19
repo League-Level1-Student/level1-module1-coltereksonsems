3888e19e46a47aaaa0a2a72a94654e2400d2cac0
root <root@879dac970647>
2022-04-19 01:27:03 +0000

    racecar

--- src/_02_race_car/RaceCarRunner.java
+++ src/_02_race_car/RaceCarRunner.java
@@ -13,11 +13,11 @@ public class RaceCarRunner {
 		// 1. Create a RaceCar and place it in 5th position
 	 RaceCar car1 = new RaceCar("Ford",5);
 		// 2. Print the RaceCar's position in the race
-
+	 System.out.println(car1.getPositionInRace());
 		// 3. Crash the RaceCar
-		
+		car1.crash();
 		// 4. If the car is damaged. Bring it in for a pit stop.
-
+if (car1 == damaged()) {car1.pit();}
 		// 5. Help the car move into first place.
 
 	}
