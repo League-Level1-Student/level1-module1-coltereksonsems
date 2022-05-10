389103d7750733a575fc74e5b849aa8dbe058224
root <root@b7abc356d880>
2022-05-10 01:29:47 +0000

    Vault

--- src/_06_vault/VaultRunner.java
+++ src/_06_vault/VaultRunner.java
@@ -5,6 +5,13 @@ public class VaultRunner {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Vault vault = new Vault();
+	System.out.println(vault.tryCode(1));
+	
+		
+		
+		
+		
 	}
 
 }
