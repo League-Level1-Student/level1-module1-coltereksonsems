d40936277ed6778b8637412a49e6a5ad4d75abbb
root <root@13be9a1580b2>
2022-06-07 01:25:01 +0000

    Start of Binary Converter

---
+++ src/_10_binary_converter/binaryconverter.java
@@ -0,0 +1,35 @@
+package _10_binary_converter;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class binaryconverter {
+public void run() {
+
+	
+JFrame frame = new JFrame();
+JPanel panel = new JPanel();
+JLabel label = new JLabel();
+	
+	frame.setVisible(true);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+	
+	
+	
+	
+}
