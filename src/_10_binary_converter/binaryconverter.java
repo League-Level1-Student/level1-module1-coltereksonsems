6a35862b7d1fbf09b6a2bff102c1989fc04d89ef
root <root@79188a1ec452>
2022-06-14 01:28:25 +0000

    binary converter mostly

--- src/_10_binary_converter/binaryconverter.java
+++ src/_10_binary_converter/binaryconverter.java
@@ -1,35 +1,60 @@
 package _10_binary_converter;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
-public class binaryconverter {
-public void run() {
+public class binaryconverter implements ActionListener {
+	JTextField answer = new JTextField(20);
+	JFrame frame = new JFrame();
+	JLabel label = new JLabel();
+	public void run() {
 
-	
-JFrame frame = new JFrame();
-JPanel panel = new JPanel();
-JLabel label = new JLabel();
-	
-	frame.setVisible(true);
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-}
-	
-	
-	
-	
+		JPanel panel = new JPanel();
+		JButton button = new JButton();
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		button.addActionListener(this);
+		frame.add(panel);
+		panel.add(answer);
+		panel.add(button);
+		panel.add(label);
+		frame.pack();
+	}
+
+	String convert(String input) {
+		if (input.length() != 8) {
+			JOptionPane.showMessageDialog(null, "Enter 8 bits, silly!!!");
+			return "-";
+		}
+		String binary = "[0-1]+";// must contain numbers in the given range
+		if (!input.matches(binary)) {
+			JOptionPane.showMessageDialog(null, "Binary can only contain 1s or 0s, silly!!!");
+			return "-";
+		}
+		try {
+			int asciiValue = Integer.parseInt(input, 2);
+			char theLetter = (char) asciiValue;
+			return "" + theLetter;
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Enter a binary, silly!!!");
+			return "-";
+		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+String answerString = answer.getText();
+String savedConvert = convert (answerString);
+label.setText(savedConvert);
+frame.pack();
+	}
+
 }
---
+++ src/_10_binary_converter/binaryconverterrunner.java
@@ -0,0 +1,11 @@
+package _10_binary_converter;
+
+public class binaryconverterrunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		binaryconverter binaryConverter = new binaryconverter();
+		binaryConverter.run();
+	}
+
+}
